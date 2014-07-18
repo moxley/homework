ce6c8e681483854b184ae40039c3765535843cba
Moxley Stratton <moxley.stratton@gmail.com>
2014-07-17 19:07:44 -0700

    Eliminate indentation level by returning early

--- app/services/search_candidates.rb
+++ app/services/search_candidates.rb
@@ -40,43 +40,43 @@ class SearchCandidates
     @job_contacts = JobContact.all(:user_id => current_user.id)
     jobs          = []
     @candidates   = []
-    unless @job_contacts.blank?
-      @job_contacts.each do |jobs_contacts|
-        @job = Job.first(:id => jobs_contacts.job_id, :is_deleted => false)
-        jobs << @job
-      end
-      jobs.each do |job|
-       unless job.blank?
-        candidate_jobs = CandidateJob.all(:job_id => job.id)
-        unless candidate_jobs.blank?
-          candidate_jobs.each do |cj|
-            candidate = cj.candidate
-            if candidate.is_deleted == false && candidate.is_completed == true && candidate.organization_id == current_user.organization_id
-              found = false
-              unless @candidates.blank?
-                @candidates.each do |cand|
-                  if cand.email_address == candidate.email_address
-                    found = true
-                  end
+    return if @job_contacts.present?
+
+    @job_contacts.each do |jobs_contacts|
+      @job = Job.first(:id => jobs_contacts.job_id, :is_deleted => false)
+      jobs << @job
+    end
+    jobs.each do |job|
+     unless job.blank?
+      candidate_jobs = CandidateJob.all(:job_id => job.id)
+      unless candidate_jobs.blank?
+        candidate_jobs.each do |cj|
+          candidate = cj.candidate
+          if candidate.is_deleted == false && candidate.is_completed == true && candidate.organization_id == current_user.organization_id
+            found = false
+            unless @candidates.blank?
+              @candidates.each do |cand|
+                if cand.email_address == candidate.email_address
+                  found = true
                 end
               end
-              if found == false
-                @candidates << candidate
-              end
             end
-            if s_key == "Candidates Newest -> Oldest"
-              @candidates = @candidates.sort_by { |c| c.created_at }
-            elsif s_key == "Candidates Oldest -> Newest"
-              @candidates = @candidates.sort_by { |c| c.created_at }.reverse
-            elsif s_key == "Candidates A -> Z"
-              @candidates = @candidates.sort_by { |c| c.created_at }.sort! { |a, b| a.last_name <=> b.last_name }
-            elsif s_key == "Candidates Z -> A"
-              @candidates = @candidates.sort_by { |c| c.created_at }.sort! { |a, b| a.last_name <=> b.last_name }.reverse
+            if found == false
+              @candidates << candidate
             end
           end
+          if s_key == "Candidates Newest -> Oldest"
+            @candidates = @candidates.sort_by { |c| c.created_at }
+          elsif s_key == "Candidates Oldest -> Newest"
+            @candidates = @candidates.sort_by { |c| c.created_at }.reverse
+          elsif s_key == "Candidates A -> Z"
+            @candidates = @candidates.sort_by { |c| c.created_at }.sort! { |a, b| a.last_name <=> b.last_name }
+          elsif s_key == "Candidates Z -> A"
+            @candidates = @candidates.sort_by { |c| c.created_at }.sort! { |a, b| a.last_name <=> b.last_name }.reverse
+          end
         end
-       end
       end
+     end
     end
   end
 end
