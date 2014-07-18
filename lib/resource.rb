73e22682089a3bbabc3e93553b05104198268208
Moxley Stratton <moxley.stratton@gmail.com>
2014-07-18 09:52:07 -0700

    Remove unused code

--- lib/resource.rb
+++ lib/resource.rb
@@ -1,14 +1,7 @@
 class Resource
-  attr_accessor :parent,
-                :children,
-                :name
+  attr_accessor :name
 
   def initialize(name)
     self.name = name
-    self.children = []
-  end
-
-  def root?
-    !parent
   end
 end
