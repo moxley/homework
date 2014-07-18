d46695ba6e2d5305b602d52aa6983fac67df47c2
Moxley Stratton <moxley.stratton@gmail.com>
2014-07-18 10:05:07 -0700

    Sample ActiveRecord code and suggested table schema

--- lib/resource_queries.rb
+++ lib/resource_queries.rb
@@ -1,6 +1,20 @@
 class ResourceQueries
   # Results are ordered by hierarchy level, ascending
   def resource_users(user, resource_name)
-    []
+    ru = ResourceUser.where(user_id: user.id)
+      joins(:resources).
+      where(resource: {name: resource_name}).
+      first
+
+    return [] unless ru
+
+    # The resource_users table has "lft" and "rgt" columns
+    # that follow the "nested set" model for hierarchical data
+    # in relational databases.
+    # See http://mikehillyer.com/articles/managing-hierarchical-data-in-mysql/
+    ResourceUser.
+      joins(:resources).
+      where('resources.lft <= ? AND resources.rgt => ?', ru.lft, ru.rgt).
+      order('resources.lft')
   end
 end
